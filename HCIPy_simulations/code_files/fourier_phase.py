be9900e1b47167abb22e3ebf6ac51ab48d15d657
Erin Holdorf <holdorf0105@gmail.com>
2023-09-07 10:32:14 +1000

    updating simulations

--- HCIPy_simulations/code_files/fourier_phase.py
+++ HCIPy_simulations/code_files/fourier_phase.py
@@ -3,28 +3,18 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import scipy.fft
 from skimage.restoration import unwrap_phase
+import cmath
 
-orig = np.load('/home/ehold13/PhD/HCIPy_simulations/output_files/initial_m1_n1.npy')
-pdi = np.load('/home/ehold13/PhD/HCIPy_simulations/output_files/final_m1_n1.npy') 
-spherical = np.load('/home/ehold13/PhD/HCIPy_simulations/output_files/spherical_m1_n1.npy')
-
-
-plt.figure()
-plt.imshow(np.angle(orig))
-
-plt.figure()
-plt.imshow(np.abs(pdi)**2)
-
-plt.figure()
-plt.imshow(np.abs(spherical)**2)
-plt.show()
+orig = np.load('/home/ehold13/PhD/HCIPy_simulations/output_files/initial_m-2_n2.npy') 
+pdi = np.load('/home/ehold13/PhD/HCIPy_simulations/output_files/final_m-2_n2.npy') 
+spherical = np.load('/home/ehold13/PhD/HCIPy_simulations/output_files/spherical_m-2_n2.npy')
 
 if True:
 
     # take the fourier transform of pdi intensity
 
     I_pdi = np.abs(pdi)**2
-
+    
     fft = scipy.fft.fftshift(scipy.fft.fft(I_pdi,axis=1))
 
     if True:
@@ -33,27 +23,68 @@ if True:
 
 
         plt.figure()
-        plt.imshow(np.abs(fft))
+        plt.imshow(np.abs(fft)**2,norm=LogNorm())
         plt.show()
-
-    fft[:,:1330] = 0
-    fft[:,1530:]=0
     
-    fft_shifted = scipy.fft.ifftshift(fft)
+    first_init = 1215
+    last_init = 1245
+    fft[:,:first_init] = 0
+    fft[:,last_init:]=0
+        
+    fft_shifted = np.zeros_like(fft)
+    
+    mid_int = int(len(fft_shifted)/2)
+    len_phase = last_init-first_init
+    
+    fft_shifted[:,int(mid_int-len_phase/2):int(mid_int+len_phase/2)] = fft[:,first_init:last_init]
+    
+    fft_shifted_ifft = scipy.fft.ifftshift(fft)
     
 
-    ifft = scipy.fft.ifft(fft_shifted,axis=1)
+    ifft = scipy.fft.ifft(fft_shifted_ifft,axis=1)
+    
 
     ifft = np.imag(np.log(ifft))
+    
+    np.save('/home/ehold13/PhD/HCIPy_simulations/output_files/left_background_phase.npy',ifft)
+    
 
     plt.figure()
     plt.imshow(ifft)
-
+    
     plt.figure()
     plt.imshow(np.angle(pdi))
 
     plt.figure()
-    plt.imshow(np.angle(orig))
+    plt.imshow(np.angle(pdi)-np.angle(ifft))
+    
+    first_init = 1155
+    last_init = 1185
+    fft[:,:first_init] = 0
+    fft[:,last_init:]=0
+        
+    fft_shifted = np.zeros_like(fft)
+    
+    mid_int = int(len(fft_shifted)/2)
+    len_phase = last_init-first_init
+    
+    fft_shifted[:,int(mid_int-len_phase/2):int(mid_int+len_phase/2)] = fft[:,first_init:last_init]
+    
+    fft_shifted_ifft = scipy.fft.ifftshift(fft)
+    
+
+    ifft = scipy.fft.ifft(fft_shifted_ifft,axis=1)
+    
+
+    ifft = np.imag(np.log(ifft))
+    
+
+    plt.figure()
+    plt.imshow(ifft)
+    
+    plt.figure()
+    plt.imshow(np.angle(pdi)-np.angle(ifft))
+    
     plt.show()
 
 
--- HCIPy_simulations/code_files/main_funcs/pdi_2d.py
+++ HCIPy_simulations/code_files/main_funcs/pdi_2d.py
@@ -4,171 +4,175 @@ from diffractio.scalar_sources_XY import Scalar_source_XY
 from diffractio.scalar_masks_XYZ import Scalar_mask_XYZ
 from diffractio.scalar_fields_XYZ import Scalar_field_XYZ
 from diffractio.vector_sources_XY import Vector_source_XY
-from diffractio.utils_drawing import draw_several_fields
+from diffractio.utils_drawing import draw_several_fields    
 
-if False:
-    # define important values
-    wavelength = 0.589*um
-    length_xy,length_z = 50*um,10*um
-    num_pix_xy,num_pix_z = 2**9, 2**9
-    width_plate = 0.5*um
-    radius_pinhole = 0.25*um
+# helper functions
 
-    # define the x,y,z space
-    xs = np.linspace(-length_xy/2,length_xy/2,num_pix_xy)
-    ys = np.linspace(-length_xy/2,length_xy/2,num_pix_xy)
-    zs = np.linspace(0,length_z,num_pix_z)
+def rebin(in_array,ratio):
+    '''
+    Function which will rebin the input array to be ratio times smaller than
+    the initial array in x and y by summing the data.
+    '''
+    
+    if ratio == 1:
+        return in_array
 
-    # define the source
-    u0 = Scalar_source_XY(xs,ys,wavelength)
-    u0.plane_wave(A=10)
-
-    # make the pinhole
-
-    # start with the semi-transparent plate
-    t0 = Scalar_mask_XYZ(xs,ys,zs,wavelength)
-    t0.square(r0=(0*um,0*um,0*um),length = (length_xy,length_xy,width_plate),refraction_index=1.5+1.0j)
-
-    t1 = Scalar_mask_XYZ(xs,ys,zs,wavelength)
-    t1.cylinder(r0=(0*um,0*um,0*um),radius = (radius_pinhole,radius_pinhole),length=width_plate,axis='z',refraction_index=1.0,angle=0)
+    out_array = np.zeros((int(np.shape(in_array)[0]/ratio),int(np.shape(in_array)[1]/ratio)))
     
-    t3 = t0+t1
-    t3.incident_field(u0)
-    t3.fast=True
-
-    t3.RS(verbose=True)
-
-
-    draw_several_fields((t3,n[:,2**7,:],abs(t3.u[:,2**7,:])**2),titles=('Mask','Final Image'),logarithm=True)
-    plt.show()
+    for i in range(np.shape(out_array)[0]):
+        for j in range(np.shape(out_array)[1]):
+            out_array[i,j] = np.nansum(in_array[i*ratio:i*ratio+ratio,j*ratio:j*ratio+ratio])
     
-if True:
-    # define the important values for the simulation
-    wavelength = 0.589*um
-    length_xy = 1000*um
-    num_pix_xy = 8000
-    pinhole_radius = 0.5*um
-    z_dist = 500*um
-    beam_rad = 10*um
-    zernike_ns = [1]
-    zernike_ms = [1]
-    zernike_cnms = [1e-10]
-    A = 1
-
-    # define the x,y,z space
-    xs = np.linspace(-length_xy/2,length_xy/2,num_pix_xy)
-    ys = np.linspace(-length_xy/2,length_xy/2,num_pix_xy)
-
-    # define the source
-    u0 = Scalar_source_XY(xs,ys,wavelength)
-    u0.zernike_beam(A=A,r0=(0*um,0*um),radius=beam_rad,n=zernike_ns,m=zernike_ms,c_nm=zernike_cnms)
-    
-    np.save('/home/ehold13/PhD/HCIPy_simulations/output_files/initial_m'+str(zernike_ms[0])+'_n'+str(zernike_ns[0])+'.npy',u0.u)
-    
-    # define the plane where the source exists
-    t0 = Scalar_mask_XY(xs,ys,wavelength)
-    #t0.gray_scale(num_levels=1,levelMin=0.1,levelMax=0.1)
-    t0.square(r0=(0*um,0*um),size=(length_xy,length_xy),angle=0)
-    
-    t0.binarize(kind='amplitude',
-                bin_level = None,
-                level0=0.0003,
-                level1=0.0003)
-
-    t1 = Scalar_mask_XY(xs,ys,wavelength)
-    t1.circle(r0=(0*um,0*um),radius = pinhole_radius,angle=0)
-    
-    t1.binarize(kind='amplitude',
-                bin_level = None,
-                level0=0.0,
-                level1=1.21)
-
-    u2 = u0*(t0+t1)
-    u2.fast=True
-
-    u3 = u2.RS(z=z_dist,verbose= True,new_field=True)
-    
-    np.save('/home/ehold13/PhD/HCIPy_simulations/output_files/final_m'+str(zernike_ms[0])+'_n'+str(zernike_ns[0])+'.npy',u3.u)
-    
-    plt.figure()
-    draw_several_fields((t0+t1,u3),titles=('Mask','PDI Final Image'),logarithm=True)
-    
-    del u2,u3,t0,t1
-    # define the plane where the source exists
-
-    t2 = Scalar_mask_XY(xs,ys,wavelength)
-    t2.square(r0=(0*um,0*um),size=(length_xy,length_xy),angle=0)
-    
-    t2.binarize(kind='amplitude',
-                bin_level = None,
-                level0=0,
-                level1=0)
-    
-    t3 = Scalar_mask_XY(xs,ys,wavelength)
-    t3.circle(r0=(0*um,0*um),radius = pinhole_radius,angle=0)
-    t3.binarize(kind='amplitude',
-                bin_level = None,
-                level0=0.0,
-                level1=1.21)
- 
-    u4 = u0*(t2+t3)
-    u4.fast=True
-
-    u5 = u4.RS(z=z_dist,verbose= True,new_field=True)
-    
-    np.save('/home/ehold13/PhD/HCIPy_simulations/output_files/spherical_m'+str(zernike_ms[0])+'_n'+str(zernike_ns[0])+'.npy',u5.u)
-   
-    
-    plt.figure()
-    draw_several_fields((t2+t3,u5),titles=('Mask','Spherical Final Image'),logarithm=True)
-    del u4,u5,t2,t3
-    
-    plt.show()
-    
-    
-    
+    return out_array
     
 
-# double slit experiment
-if False:
-    # define the important values for the simulation
-    wavelength = 1.5*um
-    length_xy = 1.5*mm
-    num_pix_xy = 2**10
+def make_subplots(electric_field, show=False):
 
-    # define the x,y,z space
-    xs = np.linspace(-length_xy,length_xy,num_pix_xy)
-    ys = np.linspace(-0.1*mm,0.1*mm,num_pix_xy)
-
-    # define the source
-    u0 = Scalar_source_XY(xs,ys,wavelength)
-    u0.plane_wave(A=1)
-
-    slit_pos = 0.15*mm
-    slit_width = 0.05*mm
-
-    # define the plane where the source exists
-#    t0 = Scalar_mask_XY(xs,ys,wavelength)
-#    t0.square(r0=(0*um,0*um),size=(slit_width,length_xy),angle=0)
+    fig,ax = plt.subplots(1,2)
+    ax[0].imshow(abs(electric_field.u)**2)
+    ax[0].set_title('Intensity')
+    ax[1].imshow(np.angle(electric_field.u))
+    ax[1].set_title("Phase")
     
-    t1 = Scalar_mask_XY(xs,ys,wavelength)
-    t1.square(r0=(slit_pos,0*um),size=(slit_width,length_xy),angle=0)
-    
-    t2 = Scalar_mask_XY(xs,ys,wavelength)
-    t2.square(r0=(-slit_pos,0*um),size=(slit_width,length_xy),angle=0)
-    
-#    t3 = Scalar_mask_XY(xs,ys,wavelength)
-#    t3.square(r0=(2*slit_pos,0*um),size=(slit_width,length_xy),angle=0)
-#    
-#    t4 = Scalar_mask_XY(xs,ys,wavelength)
-#    t4.square(r0=(-2*slit_pos,0*um),size=(slit_width,length_xy),angle=0)
+    if show:
+        plt.show()
     
 
+# define the important values for the simulation
+wavelength = 0.589*um # wavelength of the light
+length_xy = 2*mm # length of the simulation in x and y
+length_xy_init = 0.125*length_xy 
+num_pix_xy = 40*240 # number of pixels in each direction
+pinhole_radius = 0.5*um # radius of the PDI pinhole
+z_dist = 10000*um # total distance to propagate the beam
+D_tele = 1000*mm # telescope diameter
+f = 14.8
+beam_rad = 1.22*f*wavelength # initial radius of the beam
+zernike_ns = [2] # list of zernike n co-efficients
+zernike_ms = [-2] # list of zernike m co-efficients
+zernike_cnms = [1] # coefficient of each zernike mode in radians
+A = 1 # amplitude of the wave
 
-    u2 = u0*(t1+t2)
-    u2.fast=True
+# define the x,y,z space
+xs = np.linspace(-length_xy_init/2,length_xy_init/2,num_pix_xy)
+ys = np.linspace(-length_xy_init/2,length_xy_init/2,num_pix_xy)
 
-    u3 = u2.RS(z=20*mm,verbose= True,new_field=True)
+u = Scalar_source_XY(xs,ys,wavelength)
+u.plane_wave(A=1,theta=0,phi=0)
 
-    draw_several_fields((t1+t2,u3),titles=('Mask','Final Image'),logarithm=True)
-    plt.show()
+t = Scalar_mask_XY(xs,ys,wavelength)
+t.circle(r0=(0*um,0*um),radius = beam_rad,angle=0)
+
+u *= t
+# define the source
+u0 = Scalar_source_XY(xs,ys,wavelength)
+u0.zernike_beam(A=A,r0=(0*um,0*um),radius=beam_rad,n=zernike_ns,m=zernike_ms,c_nm=zernike_cnms)
+
+u0 *= u
+
+make_subplots(u0)
+
+
+rebinned_u0 = rebin(u0.u,int(length_xy/length_xy_init))
+scaled_u0 = np.zeros_like(u0.u)
+scaled_u0[int(np.shape(scaled_u0)[0]/2-np.shape(rebinned_u0)[0]/2):int(np.shape(scaled_u0)[0]/2+np.shape(rebinned_u0)[0]/2),
+int(np.shape(scaled_u0)[1]/2-np.shape(rebinned_u0)[1]/2):int(np.shape(scaled_u0)[1]/2+np.shape(rebinned_u0)[1]/2)] = rebinned_u0
+
+xf,yf = np.linspace(-length_xy/2,length_xy/2,num_pix_xy),np.linspace(-length_xy/2,length_xy/2,num_pix_xy)
+u7 = Scalar_source_XY(xf,yf,wavelength)
+
+u7.u = scaled_u0
+
+u7.fast = False
+
+u6 = u7.RS(z=z_dist,verbose= True,new_field=True)
+
+#draw_several_fields((u0,u6),titles=('Original','Original Final Image'),logarithm=True)
+
+make_subplots(u6)
+np.save('/home/ehold13/PhD/HCIPy_simulations/output_files/initial_m'+str(zernike_ms[0])+'_n'+str(zernike_ns[0])+'.npy',u6.u)
+# define the pinhole plate
+t0 = Scalar_mask_XY(xs,ys,wavelength)
+t0.square(r0=(0*um,0*um),size=(length_xy,length_xy),angle=0)
+amp = 0.005
+t0.binarize(kind='amplitude',
+            bin_level = None,
+            level0=amp,
+            level1=amp)
+
+t1 = Scalar_mask_XY(xs,ys,wavelength)
+t1.circle(r0=(0*um,0*um),radius = pinhole_radius,angle=0)
+
+t1.binarize(kind='amplitude',
+            bin_level = None,
+            level0=0.0,
+            level1=1-amp)           
+t1 += t0
+
+u2 = u0*t1
+
+
+
+rebinned_u2 = rebin(u2.u,int(length_xy/length_xy_init))
+scaled_u2 = np.zeros_like(u2.u)
+scaled_u2[int(np.shape(scaled_u2)[0]/2-np.shape(rebinned_u2)[0]/2):int(np.shape(scaled_u2)[0]/2+np.shape(rebinned_u2)[0]/2),
+int(np.shape(scaled_u2)[1]/2-np.shape(rebinned_u2)[1]/2):int(np.shape(scaled_u2)[1]/2+np.shape(rebinned_u2)[1]/2)] = rebinned_u2
+
+xf,yf = np.linspace(-length_xy/2,length_xy/2,num_pix_xy),np.linspace(-length_xy/2,length_xy/2,num_pix_xy)
+u2 = Scalar_source_XY(xf,yf,wavelength)
+
+u2.u = scaled_u2
+
+u2.fast=False
+
+u3 = u2.RS(z=z_dist,verbose= True,new_field=True)
+
+u3_detector = rebin(u3.u,int(np.shape(u3.u)[0]/240))
+np.save('/home/ehold13/PhD/HCIPy_simulations/output_files/final_m'+str(zernike_ms[0])+'_n'+str(zernike_ns[0])+'.npy',u3.u)
+
+#draw_several_fields((t1,u3),titles=('Mask','PDI Final Image'),logarithm=True)
+
+make_subplots(u3)
+
+del u2,u3,t0,t1
+
+
+# repeat above, but just ot look at the generated circular reference wavefront
+
+t2 = Scalar_mask_XY(xs,ys,wavelength)
+t2.square(r0=(0*um,0*um),size=(length_xy,length_xy),angle=0)
+
+t2.binarize(kind='amplitude',
+            bin_level = None,
+            level0=0,
+            level1=0)
+
+t3 = Scalar_mask_XY(xs,ys,wavelength)
+t3.circle(r0=(0*um,0*um),radius = pinhole_radius,angle=0)
+
+t3 += t2
+
+u4 = u0*t3
+
+rebinned_u4 = rebin(u4.u,int(length_xy/length_xy_init))
+scaled_u4 = np.zeros_like(u4.u)
+scaled_u4[int(np.shape(scaled_u4)[0]/2-np.shape(rebinned_u4)[0]/2):int(np.shape(scaled_u4)[0]/2+np.shape(rebinned_u4)[0]/2),
+int(np.shape(scaled_u4)[1]/2-np.shape(rebinned_u4)[1]/2):int(np.shape(scaled_u4)[1]/2+np.shape(rebinned_u4)[1]/2)] = rebinned_u4
+
+u4 = Scalar_source_XY(xf,yf,wavelength)
+
+u4.u = scaled_u4
+
+u4.fast=False
+
+u5 = u4.RS(z=z_dist,verbose= True,new_field=True)
+
+u5_detector = rebin(u5.u,int(np.shape(u5.u)[0]/240))
+
+np.save('/home/ehold13/PhD/HCIPy_simulations/output_files/spherical_m'+str(zernike_ms[0])+'_n'+str(zernike_ns[0])+'.npy',u5.u)
+
+make_subplots(u5)
+del u4,u5,t2,t3
+
+plt.show()
+
